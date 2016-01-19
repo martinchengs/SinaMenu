dd64a9a1fa6d3ee2755348d5c7eedd859133ddf9
AndWay <andway@AndWaydeMacBook-Air.local>
2016-01-19 11:45:55 +0800

    Initial commit

---
+++ app/src/main/java/com/szswj/sinamenu/SJToggleMenuLayout.java
@@ -0,0 +1,301 @@
+package com.szswj.sinamenu;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.OvershootInterpolator;
+import android.widget.Toast;
+
+import com.nineoldandroids.animation.Animator;
+import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by WayneSuper on 18/01/2016.
+ */
+public class SJToggleMenuLayout extends ViewGroup implements View.OnClickListener {
+    private static final String tag = "SJToggleMenuLayout";
+    private static final int MARGIN_BOTTOM = 200;
+    private static final long OPEN_DURATION = 500;
+    private final AnticipateInterpolator anticipation;
+    private final OvershootInterpolator overshoot;
+    private View mCloseButton;
+    private ArrayList<View> mChildViews;
+    private int childSize;
+    private SJToggleMenuOverlay mParent;
+    private ArrayList<Translation> mLocations;
+
+
+    private static final float INTERPOLATOR_WEIGHT = 2.5f;
+    private boolean mAnimating;
+    private boolean mExpanded = true;
+    private SJToggleMenuOverlay.OnItemClickListener listener;
+
+
+    public SJToggleMenuLayout(Context context) {
+        this(context, null);
+    }
+
+    public SJToggleMenuLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+
+    }
+
+    public SJToggleMenuLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        anticipation = new AnticipateInterpolator(INTERPOLATOR_WEIGHT);
+        overshoot = new OvershootInterpolator(INTERPOLATOR_WEIGHT);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int count = getChildCount();
+        for (int i = 0; i < count; i++) {
+            measureChild(getChildAt(i), widthMeasureSpec, heightMeasureSpec);
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    public void setMenuOverlay(SJToggleMenuOverlay overlay) {
+        this.mParent = overlay;
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        inflateSubViews();
+    }
+
+    private void inflateSubViews() {
+        int num = getChildCount();
+        if (num == 0) throw new IllegalArgumentException("the parent is empty");
+        mCloseButton = getChildAt(0);
+        mCloseButton.setOnClickListener(this);
+        mChildViews = new ArrayList<>();
+        for (int i = 1; i < num; i++) {
+            View childView = getChildAt(i);
+            mChildViews.add(childView);
+            childView.setOnClickListener(this);
+        }
+        if (mChildViews.size() > 6) {
+            throw new IllegalArgumentException("the child can't  >6");
+        }
+        mCloseButton.bringToFront();
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        if (changed) {
+            //设置关闭按钮的位置
+            Translation ct = layoutCloseButton(l, t, r, b);
+            int nL,nT,col,row,columnCount,marginX,marginY;
+            int num = mChildViews.size();
+            mLocations = new ArrayList<>();
+            for (int i = 0; i < num; i++) {
+                childSize = mChildViews.get(0).getMeasuredWidth();
+                View childView = mChildViews.get(i);
+                switch (num) {
+                    case 2:  //两个的排列方式不同
+                    case 4:
+                        columnCount = 2;
+                        row = i / columnCount;
+                        col = i % columnCount;
+                        break;
+                    case 5:
+                        if (i < 3) {
+                            columnCount = 2;
+                            if (i == 2) {
+                                row = 1;
+                                col = 2;
+                                columnCount = 3;
+                            } else {
+                                row = i / columnCount;
+                                col = i % columnCount;
+                            }
+                        } else {
+                            columnCount = 3;
+                            row = i / columnCount;
+                            col = i % columnCount;
+                        }
+                        break;
+                    default:
+                        columnCount = 3;
+                        row = i / columnCount;
+                        col = i % columnCount;
+                        break;
+                }
+                marginX = (getMeasuredWidth() - columnCount * childSize) / (columnCount + 1);
+                marginY = childSize;
+
+                nL = marginX + col * (childSize + marginX);
+                nT = b - MARGIN_BOTTOM - mCloseButton.getMeasuredWidth() - 2 * childSize - row * (childSize + marginY);
+                childView.layout(nL, nT, nL + childSize, nT + childSize);
+
+                Translation tt = new Translation(nL - ct.getX(), nT - ct.getY());
+                mLocations.add(tt);
+
+            }
+            ViewHelper.setRotation(mCloseButton, 45);
+        }
+    }
+
+    private Translation layoutCloseButton(int l, int t, int r, int b) {
+        int nL = (r - mCloseButton.getMeasuredWidth()) / 2;
+        int nt = b - mCloseButton.getMeasuredHeight() - MARGIN_BOTTOM;
+        mCloseButton.layout(nL, nt, nL + mCloseButton.getMeasuredWidth(), nt + mCloseButton.getMeasuredHeight());
+        Translation ct = new Translation(nL, nt);
+        return ct;
+    }
+
+
+    private void expandAnimation() {
+        for (int i = 0; i < mChildViews.size(); i++) {
+            int TRANSLATION_Y = mLocations.get(i).getY();
+            int TRANSLATION_X = mLocations.get(i).getX();
+            ViewPropertyAnimator.animate(mChildViews.get(i)).setDuration(OPEN_DURATION).
+                    translationYBy(TRANSLATION_Y).translationXBy(TRANSLATION_X).setInterpolator(anticipation).setListener(ON_EXPAND_COLLAPSE_LISTENER);
+            ViewPropertyAnimator.animate(mChildViews.get(i)).alpha(1.0f).setDuration(OPEN_DURATION).start();
+        }
+        ViewPropertyAnimator.animate(mCloseButton).rotation(45).setDuration(OPEN_DURATION).start();
+    }
+
+    private void closeAnimation() {
+        for (int i = 0; i < mChildViews.size(); i++) {
+            int TRANSLATION_Y = mLocations.get(i).getY();
+            int TRANSLATION_X = mLocations.get(i).getX();
+            ViewPropertyAnimator.animate(mChildViews.get(i)).setDuration(OPEN_DURATION).
+                    translationYBy(-TRANSLATION_Y).translationXBy(-TRANSLATION_X).setInterpolator(overshoot).setListener(ON_EXPAND_COLLAPSE_LISTENER);
+            ViewPropertyAnimator.animate(mChildViews.get(i)).alpha(0.0f).setDuration(OPEN_DURATION).start();
+        }
+        ViewPropertyAnimator.animate(mCloseButton).rotation(-90).setDuration(OPEN_DURATION).start();
+    }
+
+    public void onShowing() {
+        if (!mExpanded) {
+            expandAnimation();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+       switch (v.getId()) {
+           case R.id.id_toggle_menu_close:
+               toggle();
+               break;
+           case R.id.id_toggle_menu_1:
+           case R.id.id_toggle_menu_2:
+           case R.id.id_toggle_menu_3:
+           case R.id.id_toggle_menu_4:
+           case R.id.id_toggle_menu_5:
+           case R.id.id_toggle_menu_6:
+               Toast.makeText(getContext(),"点击我干什么",1).show();
+               if (listener!=null) listener.onItemClick(v);
+               break;
+       }
+    }
+
+    /**
+     * Toggle the expandable menu button, expanding or collapsing it
+     */
+    public void toggle() {
+        if (!mAnimating) {
+            mAnimating = true;
+            if (mExpanded) {
+                closeAnimation();
+            } else {
+                expandAnimation();
+            }
+        }
+    }
+
+
+    private int ANIMATION_COUNTER;
+    /**
+     * Listener for expand and collapse animations
+     */
+    private Animator.AnimatorListener ON_EXPAND_COLLAPSE_LISTENER = new Animator.AnimatorListener() {
+        @Override
+        public void onAnimationStart(Animator animation) {
+            if (ANIMATION_COUNTER == 1) {
+                mCloseButton.setEnabled(false);
+                for (int i = 0; i < mChildViews.size(); i++) {
+                    mChildViews.get(i).setEnabled(false);
+                }
+            }
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            ANIMATION_COUNTER++;
+            if (ANIMATION_COUNTER == mChildViews.size()) {
+                mExpanded = !mExpanded;
+                if (mExpanded) {
+                    for (int i = 0; i < mChildViews.size(); i++) {
+                        mChildViews.get(i).setEnabled(true);
+                    }
+                } else {
+                    for (int i = 0; i < mChildViews.size(); i++) {
+                        mChildViews.get(i).setEnabled(false);
+                    }
+                    mParent.dismiss();
+                }
+                mAnimating = false;
+
+                mCloseButton.setEnabled(true);
+                ANIMATION_COUNTER = 0;
+
+            }
+
+        }
+
+        @Override
+        public void onAnimationCancel(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animation) {
+        }
+    };
+
+    public boolean isExpanded() {
+        return mExpanded;
+    }
+
+    public void setOnItemClickListener(SJToggleMenuOverlay.OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    private class Translation {
+        int x;
+        int y;
+
+        public Translation(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+
+        public int getY() {
+            return y;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/szswj/sinamenu/SJToggleMenuOverlay.java
@@ -0,0 +1,107 @@
+package com.szswj.sinamenu;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+
+/**
+ * Created by WayneSuper on 18/01/2016.
+ */
+public class SJToggleMenuOverlay extends ImageButton implements DialogInterface.OnKeyListener, View.OnClickListener {
+
+    private Dialog mDialog;
+    private boolean mDismissing;
+    private int contentLayoutRes;
+    private SJToggleMenuLayout mToggleMenu;
+
+    public SJToggleMenuOverlay(Context context) {
+        this(context, null);
+    }
+
+    public SJToggleMenuOverlay(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SJToggleMenuOverlay(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SJToggleMenuOverlay, defStyleAttr, 0);
+        contentLayoutRes = a.getResourceId(R.styleable.SJToggleMenuOverlay_menuLayout, -1);
+        a.recycle();
+        initialize();
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+    }
+
+    private void initialize() {
+        // We create a fake dialog which dims the screen and we display the expandable menu as content
+        mDialog = new Dialog(getContext(), android.R.style.Theme_Translucent_NoTitleBar);
+        mDialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        WindowManager.LayoutParams lp = mDialog.getWindow().getAttributes();
+        lp.dimAmount = 0.8f;//设置背景透明值
+        mDialog.getWindow().setAttributes(lp);
+
+        mToggleMenu = (SJToggleMenuLayout) LayoutInflater.from(getContext()).inflate(contentLayoutRes, null);
+        mToggleMenu.setMenuOverlay(this);
+        mDialog.setContentView(mToggleMenu);
+        mDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+               mToggleMenu.onShowing();
+            }
+        });
+
+        // Catch events when keyboard button are clicked. Used to dismiss the menu
+        // on 'back' button
+        mDialog.setOnKeyListener(this);
+
+        // Clicking this view will expand the button menu
+        setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP && !event.isCanceled() && !mDismissing) {
+            if (mToggleMenu.isExpanded()) {
+                mDismissing = true;
+                mToggleMenu.toggle();
+            }
+        }
+        return false;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == getId()) {
+            show();
+        }
+    }
+
+    public void show() {
+        mDialog.show();
+    }
+
+    public void dismiss() {
+        mDialog.dismiss();
+    }
+    /**设置点击监听*/
+    public void setOItemClickListener (OnItemClickListener listener) {
+        mToggleMenu.setOnItemClickListener(listener);
+    }
+
+    public  interface OnItemClickListener {
+        void onItemClick(View view);
+    }
+}
